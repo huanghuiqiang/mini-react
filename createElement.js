d99fe0eda4e78c12794e65c7878de91312b5a75c
Hugh 2098 <huanghuiqiang86@gmail.com>
2021-11-26 16:38:40 +0800

    feat：createElement

---
+++ createElement.js
@@ -0,0 +1,59 @@
+// const element = (
+//     <div id="foo">
+//         <a>bar</a>
+//         <b/>
+//     </div>
+// )
+
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map(child => 
+                typeof child === 'object'
+                ? child
+                : createTextElement(child)    
+            )
+        }
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue: text,
+            children: []
+        }
+    }
+}
+
+// const element = React.createElement(
+//     'div',
+//     {id: 'foo'}, 
+//     React.createElement('a', null, 'bar'), 
+//     React.createElement('b')
+// )
+
+function render(element, container) {
+
+}
+
+const Didact = {
+    createElement,
+    render
+}
+
+const element = Didact.createElement(
+    'div',
+    {id: 'foo'},
+    Didact.createElement('a', null, 'bar'),
+    Didact.createElement('b')
+)
+
+console.log(element);
+
+const container = document.getElementById('root');
+// ReactDOM.render(element, container);
+Didact.render(element, container)
