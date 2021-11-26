2b54f597e37c455618bf164f518441e127f47deb
Hugh 2098 <huanghuiqiang86@gmail.com>
2021-11-26 16:46:42 +0800

    add: assign the element props to the node

--- createElement.js
+++ createElement.js
@@ -40,7 +40,14 @@ function render(element, container) {
     const dom = 
     element.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
-    : document.createElement(element, type)
+    : document.createElement(element, type);
+
+    const isProperty = key => key !== 'children';
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach(name => {
+            dom[name] = element.props[name]
+        })
 
     element.props.children.forEach(child => 
         render(child, dom)    
