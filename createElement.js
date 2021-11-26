ae56d32ad7c9a3bb96c307c20757fa2c4f229f69
Hugh 2098 <huanghuiqiang86@gmail.com>
2021-11-26 16:41:47 +0800

    add: recursively do the same for each child

--- createElement.js
+++ createElement.js
@@ -39,6 +39,10 @@ function createTextElement(text) {
 function render(element, container) {
     const dom = document.createElement(element.type);
 
+    element.props.children.forEach(child => 
+        render(child, dom)    
+    )
+
     container.appendChild(dom);
 }
 
