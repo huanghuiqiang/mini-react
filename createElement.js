a7b0c031e23960ec91935f3c7b351b37a6246684
Hugh 2098 <huanghuiqiang86@gmail.com>
2021-11-26 16:43:23 +0800

    add: handle text element

--- createElement.js
+++ createElement.js
@@ -37,7 +37,10 @@ function createTextElement(text) {
 // )
 
 function render(element, container) {
-    const dom = document.createElement(element.type);
+    const dom = 
+    element.type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(element, type)
 
     element.props.children.forEach(child => 
         render(child, dom)    
